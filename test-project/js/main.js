5caf10ce3a4ff0e2e03046990b443e7b16e0733c
antonbelinskiy <tohaantonishe@gmail.com>
2020-11-16 13:35:33 +0200

    add new project

---
+++ test-project/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+ $('.slider__inner, .news__slider-inner').slick({     
+    nextArrow: '<button type="button" class="slick-button slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-button slick-prev"></button>',
+    infinite: false
+});
+ 
+ $('select').styler();
+
+  
+
+});
