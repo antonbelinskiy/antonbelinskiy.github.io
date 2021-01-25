1d2cd3990b47ceab6e1496e6d73577e0d9936515
antonbelinskiy <tohaantonishe@gmail.com>
2021-01-25 20:10:39 +0200

    fixed

--- ZKTeco/js/main.js
+++ ZKTeco/js/main.js
@@ -1,9 +1,24 @@
 $(function () {
-    $('.product_list-inner').slick({
-        infinite: true,
-        arrows: false,
-        dots: true,  
-    })
+    // $('.product_list-inner').slick({
+    //     arrows: false,
+    //     responsive: [
+    //         {
+    //             breakpoint: 2048,
+    //             settings: "unslick"
+    //         },
+    //         {
+    //             breakpoint: 719,
+    //             settings: {
+    //                 slidesToShow: 1,
+    //                 slidesToScroll: 1,
+    //                 infinite: true,
+    //                 arrows: false,
+    //                 dots: true,
+    //             }
+    //         },
+            
+    //     ]
+    // })
     $('.slider_inner').slick({
         infinite: true,
         fade: true,
@@ -16,34 +31,33 @@ $(function () {
         //     breakpoint: 992,
         //     settings: 'unslick'
         //   }]
-    })
-
-
-
-    $(document).ready(function () {
-        $("#close_btn").hide();
-        $(".menu_btn").click(function () {
-            $(".dropdown_menu").show();
-            $(".menu_btn").hide();
-            $("#close_btn").show()
-        });
-        $("#close_btn").click(function () {
-            $(".dropdown_menu").hide();
+    })    
+        $(document).ready(function () {
             $("#close_btn").hide();
-            $(".menu_btn").show()
+            $(".menu_btn").click(function () {
+                $(".dropdown_menu").show();
+                $(".menu_btn").hide();
+                $("#close_btn").show()
+            });
+            $("#close_btn").click(function () {
+                $(".dropdown_menu").hide();
+                $("#close_btn").hide();
+                $(".menu_btn").show()
+            });
         });
     });
-});
-$(window).on("load resize", function(){
-    var width = $(document).width();
-    
-    if (width > 992) {
-      $('.product_list-inner').slick('unslick');
-    } else {
-      $('.product_list-inner').slick({  
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-      });
-    }
-  });
+
+
+// $(window).on("load resize", function(){
+//     var width = $(document).width();
+
+//     if (width > 772 ) {
+//       $('.product_list-inner').slick('unslick');
+// } else {
+//   $('.product_list-inner').slick({  
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     infinite: true,
+//   });
+//     }
+//   });
